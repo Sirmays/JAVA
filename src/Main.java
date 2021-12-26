58fd7c82ce5fdcb6f3b24a0b50cf1635a83e3e60
Sirmays <Sirmays@1>
2021-12-26 18:44:47 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+
+        new Thread(() -> printer.PrintA()).start();
+        new Thread(() -> printer.PrintB()).start();
+        new Thread(() -> printer.PrintC()).start();
+
+
+    }
+
+
+}
